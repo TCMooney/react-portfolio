5068f913fc6e61fe43b50f10167dd07e28041287
TCMooney <tcmooney88@gmail.com>
2019-07-14 21:34:00 -0400

    built out the basics of portfolio components

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,49 @@
 import React, { Component } from 'react';
 import moment from 'moment';
+import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
+import NavigationContainer from './navigation/navigation-container';
+import Home from './pages/home';
+import About from './pages/about';
+import Contact from './pages/contact';
+import Blog from './pages/blog';
+import PortfolioDetail from './portfolio/portfolio-detail';
+import NoMatch from './pages/no-match';
 
 export default class App extends Component {
+  getPortfolioItems() {
+    axios.get("https://tommooney.devcamp.space/portfolio/portfolio_items")
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
   render() {
     return (
       <div className='app'>
-        <h1>Tom Mooney React Portfolio</h1>
-        <div>{moment().format('MMMM DO YYYY, h:mm:ss a')}</div>
+        <Router>
+          <div>
+            <h1>Tom Mooney React Portfolio</h1>
+            <div>{moment().format('MMMM DD YYYY, h:mm:ss a')}</div>
+            <NavigationContainer/>
+            <Switch>
+              <Route exact path='/' component={Home}/>
+              <Route path='/about-me' component={About}/>
+              <Route path='/contact' component={Contact}/>
+              <Route path='/blog' component={Blog}/>
+              <Route exact path='/portfolio/:slug' component={PortfolioDetail}/>
+              <Route component={NoMatch}/>
+            </Switch>
+          </div>
+        </Router>
+        
       </div>
     );
   }
