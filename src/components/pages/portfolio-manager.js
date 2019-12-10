2f3401241fd7f29bdefab4771efb75bd3e442436
TCMooney <tcmooney88@gmail.com>
2019-12-09 20:46:24 -0500

    pulled data from api and stored in it in the state

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export default class PortfolioManager extends Component {
-    render() {
+    constructor() {
+        super();
+
+        this.state = {
+            portfolioItems: []
+        }
+    }
+
+    getPortfolioItems() {
+        axios.get("https://tommooney.devcamp.space/portfolio/portfolio_items", { withCredentials: true })
+            .then(response => {
+                this.setState({
+                    portfolioItems: [...response.data.portfolio_items]
+                })
+            })
+            .catch(err => {
+                console.log("error in getPortfolioItems", err)
+            })
+    }
+
+    componentDidMount() {
+        this.getPortfolioItems()
+    }
+    render() { 
         return (
             <div className='portfolio-manager-wrapper'>
                 <div className='left-column'>
